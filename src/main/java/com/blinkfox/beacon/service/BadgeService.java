3a5f4500320ddf66b26f956caf2725a07589b53d
blinkfox <chenjiayin1990@163.com>
2019-03-30 17:28:50 +0800

    重构徽章生成的模板和参数

--- src/main/java/com/blinkfox/beacon/service/BadgeService.java
+++ src/main/java/com/blinkfox/beacon/service/BadgeService.java
@@ -31,17 +31,17 @@ public class BadgeService {
      * @return 徽章的svg字符串
      */
     public String generate(Badge badge) {
-        // 分别计算并设置该徽章 label 和 message 的16进制颜色值、徽章的风格以及徽章左右文本所需要占的宽度.
-        badge.setLabelColor(ColorKit.getHexLabelColor(badge.getLabelColor()))
-                .setColor(ColorKit.getHexColor(badge.getColor()))
-                .setStyle(StyleKit.getStyle(badge.getStyle()))
-                .setLeftWidth(TextWidthKit.calculate(badge.getLabel()))
-                .setRightWidth(TextWidthKit.calculate(badge.getMessage()));
+        // 分别计算该徽章 label 和 message 的16进制颜色值、徽章的风格以及徽章左右文本所需要占的宽度.
+        // 并将这些值存放到 map 中作为参数上下文传递给beetl, 从而渲染出svg模板.
+        Map<String, Object> contextMap = new HashMap<>(8);
+        contextMap.put("label", badge.getLabel());
+        contextMap.put("message", badge.getMessage());
+        contextMap.put("labelColor", ColorKit.getHexLabelColor(badge.getLabelColor()));
+        contextMap.put("color", ColorKit.getHexColor(badge.getColor()));
+        contextMap.put("leftWidth", TextWidthKit.calculate(badge.getLabel()));
+        contextMap.put("rightWidth", TextWidthKit.calculate(badge.getMessage()));
 
-        // 将badge对象封装到Map中
-        Map<String, Object> paramMap = new HashMap<>(4);
-        paramMap.put("b", badge);
-        return TemplateKit.render(badge.getStyle() + SVG, paramMap);
+        return TemplateKit.render(StyleKit.getStyle(badge.getStyle()) + SVG, contextMap);
     }
 
 }
